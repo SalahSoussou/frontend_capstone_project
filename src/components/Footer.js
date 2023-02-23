92b7136b0b6f091417b9779d6850e59a3b5d7656
SalalSoussou <sousssousalahiddine@gmail.com>
2023-02-23 15:52:47 +0100

    ok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import logo from "../assets/Logo.svg";
+import Nav from "./Nav";
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <img src={logo} alt="logo" />
+      <Nav />
+      <ul>
+        contact
+        <li>Address</li>
+        <li>Phone number</li>
+        <li>email</li>
+      </ul>
+      <ul>
+        Social Media Links
+        <li>Address</li>
+        <li>Phone number</li>
+        <li>email</li>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;
