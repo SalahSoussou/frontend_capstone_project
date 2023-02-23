09b2e402c55748f684d67c2ef72befa34e79285b
SalalSoussou <sousssousalahiddine@gmail.com>
2023-02-23 11:48:29 +0100

    set up components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import logo from "../assets/Logo.svg";
+import Nav from "./Nav";
 
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <img src={logo} alt="logo" />
+      <Nav />
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 function Nav() {
-  return <nav>Nav</nav>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#menu">Home</a>
+        </li>
+        <li>
+          <a href="#menu">About</a>
+        </li>
+        <li>
+          <a href="#menu">Menu</a>
+        </li>
+        <li>
+          <a href="#menu">Reservation</a>
+        </li>
+        <li>
+          <a href="#menu">Order online</a>
+        </li>
+        <li>
+          <a href="#menu">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
